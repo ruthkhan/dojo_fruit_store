5bef1e1bab191fe1ebae5d63cade3756777d08e3
Ruth Khan <ruth.khan@gmail.com>
2023-07-23 22:14:36 +0800

    modified Michael Choi's code to render template

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
+
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -7,8 +9,10 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    timestamp = datetime.now().strftime("%B %d %Y %X %p")
+    total = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    print(f"Charging {request.form['first_name']} {request.form['last_name']} for {total} fruits.")
+    return render_template("checkout.html", total=total, timestamp=timestamp)
 
 @app.route('/fruits')         
 def fruits():
